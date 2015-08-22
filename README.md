2705a89b9c8e77d8f90573f90e40bbf782493cab
w3shaman <bogeyman2007@gmail.com>
2015-08-22 15:21:43 +0700

    Update example. Update README to add detail information and live example link.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # JQuery LazzyNumeric
 JQuery Plugin for formatting numeric value with thousand separator then clear the value when submitting the form.
 This plugin extends the jQuery autoNumeric (http://www.decorplanit.com/plugin/).
+Further information can be read from here: http://w3shaman.com/article/thousand-separator-using-jquery-lazzynumeric
+
+# Demo
+The live example can be tried from here: http://stuff.w3shaman.com/jquery-plugins/lazzynumeric/
--- index.php
+++ index.php
@@ -10,13 +10,17 @@
   <h1>JQuery LazzyNumeric</h1>
   <p>The lazzy plugin to format number with thousand separator on typing. This plugin extends the <b>autoNumeric</b> plugin (<a href="http://www.decorplanit.com/plugin/" target="_blank">http://www.decorplanit.com/plugin/</a>) so when the form is submitted the number format will be cleared and you don't need to do anything to clear the format at your server side script.</p>
   <?php
-  $price1 = "";
-  $price2 = "";
+  $item_name = "";
+  $item_qty = "";
+  $item_price1 = "";
+  $item_price2 = "";
 
   // Check submitted value to see if the number format is cleared.
-  if (isset($_POST['submit'])) {
-    $price1 = $_POST["item_price1"];
-    $price2 = $_POST["item_price2"];
+  if (isset($_POST["submit"])) {
+    $item_name = $_POST["item_name"];
+    $item_qty = $_POST["item_qty"];
+    $item_price1 = $_POST["item_price1"];
+    $item_price2 = $_POST["item_price2"];
 
     echo '<div style="border:1px solid #999;background-color:#ddd">';
     echo 'Hooray the value is back to the pure number!';
@@ -28,16 +32,16 @@
   ?>  
   <form action="" method="post">
     <table>
-      <tr><td>Item name</td><td>:</td><td><input type="text" name="item_name" value="Laptop" /></td></tr>
-      <tr><td>Qty</td><td>:</td><td><input type="text" name="item_qty" /></td></tr>
-      <tr><td>Price 1</td><td>:</td><td><input type="text" name="item_price1" class="money" value="<?php echo $price1 ?>" /></td></tr>
-      <tr><td>Price 2</td><td>:</td><td><input type="text" name="item_price2" class="money" value="<?php echo $price2 ?>" /></td></tr>
+      <tr><td>Item name</td><td>:</td><td><input type="text" name="item_name" value="<?php echo $item_name ?>" /></td></tr>
+      <tr><td>Qty</td><td>:</td><td><input type="text" name="item_qty" class="numeric" value="<?php echo $item_qty ?>" /></td></tr>
+      <tr><td>Price 1</td><td>:</td><td><input type="text" name="item_price1" class="numeric" value="<?php echo $item_price1 ?>" /></td></tr>
+      <tr><td>Price 2</td><td>:</td><td><input type="text" name="item_price2" class="numeric" value="<?php echo $item_price2 ?>" /></td></tr>
     </table>
     <br/>
     <input type="submit" name="submit" id="submit" />
   </form>
   <script language="javascript" type="text/javascript">
-    $(".money").lazzynumeric();
+    $(".numeric").lazzynumeric();
   </script>
 </body>
 </html>
